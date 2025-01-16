1c2a106bc9e96bbb241d35a866374c507979d6f9
Todd Hainsworth <hainsworth.todd@gmail.com>
2025-01-16 20:35:34 +1030

    remove duplicate conform

--- lua/todd/plugins/conform.lua
+++ lua/todd/plugins/conform.lua
@@ -1,16 +1,32 @@
 return {
-  'stevearc/conform.nvim',
-  lazy = false,
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      rust = { "rustfmt", lsp_format = "fallback" },
-      typescript = { "prettierd" },
-    },
-    format_on_save = { timeout_ms = 500 },
-    log_level = vim.log.levels.DEBUG,
-  },
-  config = function(opts)
-    require('comform').setup(opts)
-  end
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+			lua = { "stylua" },
+		},
+		format_on_save = {
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 1000,
+		},
+	},
+	config = function(opts)
+		local conform = require("conform")
+
+		conform.setup(opts)
+
+		vim.keymap.set({ "n", "v" }, "<leader>f", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
