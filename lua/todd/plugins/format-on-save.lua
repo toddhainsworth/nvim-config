5f7cac79bb6c9089aedcdc2dd06bf8619fe1a8ce
Todd Hainsworth <todd.hainsworth@aligent.com.au>
2025-01-17 08:43:56 +1030

    Replace conform with format-on-save
    For some reason conform wasn't actually doing antything...but
    format-on-save does :shrug:

---
+++ lua/todd/plugins/format-on-save.lua
@@ -0,0 +1,37 @@
+return {
+  "elentok/format-on-save.nvim",
+  config = function()
+    local formatters = require('format-on-save.formatters');
+
+    require('format-on-save').setup({
+      exclude_path_patterns = {
+        "/node_modules/",
+        ".local/share/nvim/lazy",
+      },
+      formatter_by_ft = {
+        css = formatters.lsp,
+        html = formatters.lsp,
+        java = formatters.lsp,
+        javascript = formatters.lsp,
+        json = formatters.lsp,
+        lua = formatters.lsp,
+        markdown = formatters.prettierd,
+        openscad = formatters.lsp,
+        python = formatters.black,
+        rust = formatters.lsp,
+        scad = formatters.lsp,
+        scss = formatters.lsp,
+        sh = formatters.shfmt,
+        terraform = formatters.lsp,
+        typescript = formatters.prettierd,
+        typescriptreact = formatters.prettierd,
+        yaml = formatters.lsp,
+      },
+      fallback_formatter = {
+        formatters.remove_trailing_whitespace,
+        formatters.remove_trailing_newlines,
+        formatters.prettierd,
+      },
+    })
+  end
+}
